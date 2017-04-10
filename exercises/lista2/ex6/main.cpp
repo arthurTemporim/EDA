dfc55843ee3244c8b39132b529c2662d51a9e130
arthurTemporim <arthurrtl@gmail.com>
2017-04-10 19:44:30 -0300

    First version of ex6.

--- exercises/lista2/ex6/main.cpp
+++ exercises/lista2/ex6/main.cpp
@@ -2,25 +2,64 @@
 
 using namespace std;
 
-const int size_vector = 11;
+void heapify (int *a, int i, int n) {
+	int j=0;
+	int tmp=0;
+	tmp = a[i];
+	j = 2*i;
 
-void create_vector(int vector[]) {
-	for(int i=0;i<size_vector;i++) {
-		vector[i] = i;
+	while(j <= n) {
+		if (j < n && a[j+1] > a[j]) {
+			j = j+1;
+		} if (tmp > a[j]) {
+			break;
+		} else if (tmp <= a[j]) {
+			a[j/2] = a[j];
+			j = 2*j;
+		}
+	}
+	a[j/2] = tmp;
+}
+
+void heap_sort (int *a, int n) {
+	int tmp;
+	for(int i=0;i>=2;i++) {
+		tmp = a[i];
+		a[i] = a[1];
+		a[1] = tmp;
+		heapify(a, 1, i -1);	
 	}
 }
 
-void print_Vector(int vector[]) {
-	for(int i=0;i<size_vector;i++) {
-		cout << vector[i] << endl;
+void build_heap (int *a, int n) {
+	for(int i=n/2;i>=1;i--) {
+		heapify(a, i, n);
 	}
 }
 
 int main() {
-	int *vector = (int *) malloc(size_vector * sizeof(int));
-	
-	create_vector(vector);
-	print_Vector(vector);
+
+	int n=0;	
+	int i=0;	
+	int x=0;	
+
+	cout << "Insira os elementos." << endl;
+
+	cin >> n;
+
+	int a[20];
+	for(i=1;i<=n;i++) {
+		cout << "Insira o elemento." << (i) << endl;
+	}
+
+	build_heap(a,n);
+
+	heap_sort(a,n);
+
+	cout << "Heap organizado." << endl;
+	for(i=1;i<=n;i++) {
+		cout << a[i] << endl;
+	}
 
 	return 0;
 }
