ce506b4e4743fa4c0056aaf774c33cb378c9de84
brunobrg <bbraganca1991@gmail.com>
2017-04-10 20:41:24 -0300

    swap sendo feito, primeiro node = posicao mais inical, segundo node = posicao mais final

--- exercises/lista2/ex5/main.c
+++ exercises/lista2/ex5/main.c
@@ -14,14 +14,16 @@ struct _node {
 };
 
 struct _list {
+  int count;
   Node *head;
   Node *tail;
 };
 
 int add_value(List**, int);
 Node *add_node(int);
-int swap_node(Node**, Node**);
+int swap_node(List *, Node*, Node*);
 void print(Node*);
+void print_end(Node*);
 
 int main (int argc, char *argv[]) {
   List *list;
@@ -33,6 +35,8 @@ int main (int argc, char *argv[]) {
   for (iterator = 0; iterator < NUMBER_VALUES; iterator++) {
     add_value(&list,  rand()%100 + 1);
   }
+  print(list->head);
+  swap_node(list, list->head, list->head->next);
   print(list->head);
   return 1;
 }
@@ -53,21 +57,84 @@ int add_value(List **list, int value) {
     *list = (List*)malloc(sizeof(List));
     (*list)->head = node;
     (*list)->tail = node;
+    (*list)->count = 1;
   } else {
-    Node *aux = (*list)->tail;
-    while(aux->next != NULL)
-      aux = aux->next;
-    aux->next = node;
-    aux->next->previous = aux;
-
-    (*list)->tail = aux->next;
+    (*list)->tail->next = node;
+    (*list)->tail->next->previous = (*list)->tail;
+    (*list)->tail = (*list)->tail->next;
+    (*list)->count += 1;
   }
+}
+
+int swap_node(List *list, Node *first_node, Node *second_node) {
+  Node *first_previous = first_node->previous;
+  Node *first_next = first_node->next;
+
+  first_node->previous = second_node->previous;
+  first_node->next = second_node->next;
+
+  second_node->previous = first_previous;
+  second_node->next = first_next;
+
+  if (first_node->previous == first_node) {
+    first_node->previous = second_node;
+  } else {
+    // do nothing
+  }
+  if (second_node->next == second_node) {
+    second_node->next = first_node;
+  }
+
+  if (first_node->next != NULL) {
+    first_node->next->previous = first_node;
+  } else {
+    list->tail = first_node;
+  }
+  first_node->previous->next = first_node;
+
+  if (second_node->previous != NULL) {
+    second_node->previous->next = second_node;
+  } else {
+    list->head = second_node;
+  }
+  second_node->next->previous = second_node;
+  
+  // first_node->next = second_node->next;
+  // if (first_node->next != NULL) {
+  //   first_node->next->previous = first_node;
+  // } else {
+  //   list->tail = first_node;
+  // }
+  //
+  // first_node->previous = second_node->previous;
+  // first_node->previous->next = first_node;
+  // second_node->previous = first_previous;
+  // if (first_previous != NULL) {
+  //   second_node->previous->next = second_node;
+  // } else {
+  //   list->head = second_node;
+  // }
+  // second_node->next = first_next;
+  // second_node->next->previous = second_node;
+
+  return 1;
 }
 
 void print(Node *tmp) {
   if (tmp != NULL) {
     printf("%d ", tmp->value);
     print(tmp->next);
+  } else {
+    printf("\n");
+  }
+
+  // exiting print
+}
+
+void print_end(Node *tmp) {
+  if (tmp != NULL) {
+    printf("%d ", tmp->value);
+    print_end(tmp->previous);
   } else {
     printf("\n");
   }
