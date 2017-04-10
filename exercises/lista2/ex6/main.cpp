4ba1c73f9673ba8d4a1ec2036be23d45c2b6ec86
arthurTemporim <arthurrtl@gmail.com>
2017-04-10 19:51:40 -0300

    Second version of ex6 made.

--- exercises/lista2/ex6/main.cpp
+++ exercises/lista2/ex6/main.cpp
@@ -2,63 +2,67 @@
 
 using namespace std;
 
-void heapify (int *a, int i, int n) {
-	int j=0;
-	int tmp=0;
-	tmp = a[i];
+void max_heapify(int *a, int i, int n) {
+
+	int j, temp;
+	temp = a[i];
 	j = 2*i;
 
-	while(j <= n) {
+	while (j <= n) {
+
 		if (j < n && a[j+1] > a[j]) {
 			j = j+1;
-		} if (tmp > a[j]) {
+		} if (temp > a[j]) {
 			break;
-		} else if (tmp <= a[j]) {
+		} else if (temp <= a[j]) {
 			a[j/2] = a[j];
 			j = 2*j;
 		}
 	}
-	a[j/2] = tmp;
+
+	a[j/2] = temp;
+	return;
 }
 
-void heap_sort (int *a, int n) {
-	int tmp;
-	for(int i=0;i>=2;i++) {
-		tmp = a[i];
+void heapsort(int *a, int n) {
+
+	int i, temp;
+	for (i = n; i >= 2; i--) {
+
+		temp = a[i];
 		a[i] = a[1];
-		a[1] = tmp;
-		heapify(a, 1, i -1);	
+		a[1] = temp;
+
+		max_heapify(a, 1, i - 1);
 	}
 }
 
-void build_heap (int *a, int n) {
-	for(int i=n/2;i>=1;i--) {
-		heapify(a, i, n);
+void build_maxheap(int *a, int n) {
+
+	int i;
+	for(i = n/2; i >= 1; i--) {
+		max_heapify(a, i, n);
 	}
 }
 
 int main() {
 
-	int n=0;	
-	int i=0;	
-	int x=0;	
-
-	cout << "Insira os elementos." << endl;
-
-	cin >> n;
+	int n, i, x;
+	cout<<"Enter no of elements of array" << endl;
+	cin>>n;
 
 	int a[20];
-	for(i=1;i<=n;i++) {
-		cout << "Insira o elemento." << (i) << endl;
+	for (i = 1; i <= n; i++) {
+
+		cout<<"Enter element "<< i<<endl;
+		cin>>a[i];
 	}
 
-	build_heap(a,n);
-
-	heap_sort(a,n);
-
-	cout << "Heap organizado." << endl;
-	for(i=1;i<=n;i++) {
-		cout << a[i] << endl;
+	build_maxheap(a,n);
+	heapsort(a, n);
+	cout<<"sorted output" << endl;
+	for (i = 1; i <= n; i++) {
+		cout<<a[i]<<endl;
 	}
 
 	return 0;
